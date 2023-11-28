4296218291571eb48c450dcea4242c7e123d2c39
Ribich1 <ribich@ukr.net>
2023-11-28 21:45:55 +0100

    init project

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Yevhen:QzOoDlnZHbiRP0Tx@cluster0.oheb2op.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful"),
+      app.listen(3000, () => {
+        console.log("Server running. Use our API on port: 3000");
+      });
+  })
+  .catch((error) => console.log(error.message), process.exit(1));
