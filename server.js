bd3d0045b066eea13b320f015436366c5becdd2a
Ribich1 <ribich@ukr.net>
2023-11-28 23:30:59 +0100

    add PORT to .env and variables

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 import app from "./app.js";
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT=3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
