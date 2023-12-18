f0d374f7d15a0f056c63b0935f7322265201cc34
Ribich1 <ribich@ukr.net>
2023-12-18 15:33:58 +0100

    fixed file not found

--- middlewares/jimp.js
+++ middlewares/jimp.js
@@ -4,6 +4,10 @@ import path from "path";
 import jimp from "jimp";
 
 async function mainJimpResize(req, res, next) {
+  if (!req.file) {
+    return next(HttpError(404, "File not found"));
+  }
+
   const { path: tempPathAvatar } = req.file;
 
   const image = await jimp.read(tempPathAvatar);
